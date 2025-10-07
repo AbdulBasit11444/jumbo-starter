a212a848ab76ae42f21a0cf53c80e4a309f1eebd
AbdulBasit11444 <basit11444@gmail.com>
2025-10-07 11:26:28 +0500

    project upload and login update

---
+++ src/app/_translations/en.js
@@ -0,0 +1,25 @@
+export const enTranslation = {
+  sidebar: {
+    menu: {
+      sample: "Sample Menu",
+    },
+    menuItem: {
+      sample: "Sample Item",
+    },
+  },
+  login: {
+    jumbo: "Jumbo",
+    loginEmail: "Email or Username",
+    email: "Email",
+    password: "Password",
+    rememberMe: "Remeber Me",
+    signIn: "Login",
+    content: "By signing in, you can avail full features of the Jumbo.",
+    forgot1: "Forgot your password? Recover Now",
+    forgotPassword: "Forgot your password?",
+    recover: "Register now",
+    other: "Or sign up with",
+    loggedIn: "Login",
+    account: "Don't have an account?",
+  },
+};
